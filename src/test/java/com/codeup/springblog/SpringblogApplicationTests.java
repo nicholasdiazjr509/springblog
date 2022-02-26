30da9e0b3a9b9fe63456bd7ae202e8c742f41b1a
Nicholas Diaz <nicholasdiazjr509@gmail.com>
2022-02-26 13:22:25 -0600

    somehow I forgot SpringblogApplication.java file

--- src/test/java/com/codeup/springblog/SpringblogApplicationTests.java
+++ src/test/java/com/codeup/springblog/SpringblogApplicationTests.java
@@ -2,12 +2,11 @@ package com.codeup.springblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//@EnableJpaRepositories(basePackages = {"com.codeup.springblog"})
 @SpringBootTest
 class SpringblogApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
